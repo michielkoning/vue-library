9bf01f097c8320c1cce4749dd524c71c4abf87b9
Michiel Koning <michiel.koning@valtech.com>
2020-10-01 17:04:23 +0200

    first commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from "vue";
+import { AppButton } from "@/entry";
+
+export default Vue.extend({
+  name: "ServeDev",
+  components: {
+    AppButton,
+  },
+});
+</script>
+
+<template>
+  <div id="app">
+    <AppButton />
+  </div>
+</template>
